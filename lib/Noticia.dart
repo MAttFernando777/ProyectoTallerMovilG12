e9dc6ced3f6483b304a3352f1d0e50db157c1a47
Valeria Llerena <valeryjhoanna@hotmail.com>
2023-09-30 19:15:35 -0500

    SE AGREGARON Noticia.dart POR VALERIA.

--- lib/Noticia.dart
+++ lib/Noticia.dart
@@ -9,8 +9,105 @@ class Noticia extends StatelessWidget {
       drawer: MenuLateral(),
       appBar: AppBar(),
       body: Center(
-        child: Text("ESTÁS EN NOTICIAS"),
+        child: Card(
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.red,
+                child: Center(child: Text('Noticias')),
+              ),
+              const ListTile(
+                leading: Icon(Icons.festival),
+                title: Text("PRIMERA NOTICIA"),
+                subtitle: Text("SUBTITULO PRIMERA NOTICIA"),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text("LEER MAS..."),
+                    onPressed: (){},
+                  ),
+
+                ]
+              ),
+              Divider(),
+              //DE AQUI A MAS ABAJO SE REPITE LO DE ARRIBA.
+              const ListTile(
+                leading: Icon(Icons.festival),
+                title: Text("SEGUNDA NOTICIA"),
+                subtitle: Text("SUBTITULO SEGUNDA NOTICIA"),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text("LEER MAS..."),
+                      onPressed: (){},
+                    ),
+
+                  ]
+              ),
+              Divider(),
+              const ListTile(
+                leading: Icon(Icons.festival),
+                title: Text("TERCERA NOTICIA"),
+                subtitle: Text("SUBTITULO TERCERA NOTICIA"),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text("LEER MAS..."),
+                      onPressed: (){},
+                    ),
+
+                  ]
+              ),
+              Divider(),
+              const ListTile(
+                leading: Icon(Icons.festival),
+                title: Text("CUARTA NOTICIA"),
+                subtitle: Text("CUARTA TERCERA NOTICIA"),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text("LEER MAS..."),
+                      onPressed: (){},
+                    ),
+
+                  ]
+              ),
+              Divider(),
+              const ListTile(
+                leading: Icon(Icons.festival),
+                title: Text("QUINTA NOTICIA"),
+                subtitle: Text("SUBTITULO QUINTA NOTICIA"),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text("LEER MAS..."),
+                      onPressed: (){},
+                    ),
+
+                  ]
+              ),
+
+
+            ]
+          )
+        ),
+
       ),
+
       bottomNavigationBar: Footer(),
     );
   }
